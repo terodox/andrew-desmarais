6c669ae2f4e9d2d0ab567226de912e2d38e76166
adesmarais <terodox@gmail.com>
2017-03-17 20:17:20 -0400

    One more tweak. Spacing is important.

--- app/experience/experience.js
+++ app/experience/experience.js
@@ -14,7 +14,7 @@ angular.module('andrewDesmaraisApp.experience', ['ngRoute'])
       {
           company: "Newforma",
           logoUrl: "https://lh3.googleusercontent.com/Cr1Euyncr5ExcWRXgG4j-Zf-dIpRzzDjE6BlvjYPwBoJr3mQNwyA53bKZi_IkJI6Yu0=w300",
-          title: "Principal Engineer/Technical Lead",
+          title: "Principal Engineer / Technical Lead",
           fromDateString: "September 2014",
           toDateString: "Present",
           summary: "Lead a team to develop and maintain software including, but not limited to, C# based APIs, AngularJs based website, NodeJs and Java based AWS lambda functions.",
@@ -30,7 +30,7 @@ angular.module('andrewDesmaraisApp.experience', ['ngRoute'])
       {
           company: "CustomScoop",
           logoUrl: "https://pbs.twimg.com/profile_images/3282187373/09f97ee69a31621313eb8f00881020da_400x400.png",
-          title: "Director of Web Development/Senior Full Stack Developer",
+          title: "Director of Web Development / Senior Full Stack Developer",
           fromDateString: "2006",
           toDateString: "September 2014",
           summary: "Develop and maintain software-as-a-service suite that facilitates the finding, management and reporting of keyword matches. Work to create flexible and scalable solutions allowing for continuous growth and prosperity of the company as a whole.",
@@ -43,7 +43,7 @@ angular.module('andrewDesmaraisApp.experience', ['ngRoute'])
       {
           company: "Skillsoft",
           logoUrl: "https://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/0024/3655/brand.gif?itok=KYH1zM8K",
-          title: "Hosting Engineer/Application Engineer",
+          title: "Hosting Engineer / Application Engineer",
           fromDateString: "2004",
           toDateString: "2006",
           summary: "Racked and configured servers to facilitate the expanding customer base while maintaining a high level of stability throughout the product. Worked with development team to implement and maintain processes ensuring the continued high level of uptime.",
