d806e706f030a5b44a06006e1fdd5c732df7610b
terodox <terodox@gmail.com>
2019-05-02 21:22:50 -0400

    Consolidate to single file

--- docs/quad-view/quad-view.js
+++ docs/quad-view/quad-view.js
@@ -1,1 +1,40 @@
-import './shared-pane.js';
+export const template = `
+<style>
+    :host {
+        width: .5vw;
+        height: .5vh;
+        margin: 0;
+        padding: 0;
+        display: inline-block;
+
+        font-family: 'Helvetiva Neue', Arial, Helvetica, sans-serif;
+        font-size: 12px;
+    }
+    .url-bar {
+        width: 100%;
+    }
+</style>
+<div>
+    <input class="url-bar">
+</div>
+<iframe></iframe>
+`;
+
+class SharedPane extends HTMLElement {
+    static get tagName() {
+        return 'shared-pane';
+    }
+
+    connectedCallback() {
+        this.id = this.getAttribute('id');
+        if(!this.id) {
+            throw new Error('An id is required for shared-pane to function');
+        }
+
+        this.root = this.attachShadow({ mode: 'open' });
+
+        this.root.innerHTML = template;
+    }
+}
+
+customElements.define(SharedPane.tagName, SharedPane);
