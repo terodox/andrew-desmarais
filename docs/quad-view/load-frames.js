c4f18a36f3b07e2c460d46da1964e74b822c5da5
terodox <terodox@gmail.com>
2019-05-02 22:05:11 -0400

    Make the frame load something

--- docs/quad-view/load-frames.js
+++ docs/quad-view/load-frames.js
@@ -1,2 +1,5 @@
-const upperLeft = document.getElementById('upper-left');
-console.log(upperLeft);
+function loadTheFrame() {
+    const upperLeft = document.getElementById('upper-left');
+    console.log(upperLeft);
+    upperLeft.src = 'https://giphy.com/gifs/garyvee-microphone-gary-vaynerchuk-is-this-thing-on-f9SgNsok0zsxI3vS56/fullscreen';
+}
