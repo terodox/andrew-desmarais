83f25e9508587f7104b17bac654e204ee0ca1720
terodox <terodox@gmail.com>
2020-03-05 13:45:00 -0500

    Lila first pass

---
+++ docs/lila/app.js
@@ -0,0 +1,26 @@
+const allLeftPanelDivs = document.querySelectorAll('.left-panel div');
+allLeftPanelDivs.forEach(function (el) {
+    el.addEventListener('click', function(event) {
+        allLeftPanelDivs.forEach(function (leftPanelDiv) {
+            leftPanelDiv.classList.remove('selected');
+        });
+        event.srcElement.classList.add('selected');
+    });
+});
+
+document.querySelectorAll('.row div').forEach(function (el) {
+    el.addEventListener('click', function(event) {
+        const tileColor = document.querySelector('div.selected');
+        const availableClasses = [ ...tileColor.classList.values() ];
+        const classToAdd = availableClasses.filter(className => className !== 'selected');
+        event.srcElement.className = '';
+        event.srcElement.classList.add(classToAdd);
+    });
+});
+
+// Make player draggable
+const tileContainer = document.querySelector('tile-container');
+const draggable = new PlainDraggable(document.getElementById('player', {
+    containment: tileContainer,
+}));
+draggable.snap = { step: 50 };
